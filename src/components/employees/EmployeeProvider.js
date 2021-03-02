c28fb09cd5208b6af7278eb2a4850b5ca482360a
kjk1325e <kaitlinjkelley@gmail.comm>
2021-03-02 09:50:39 -0600

    Completed EmployeeProvider component, including getEmployyes and addEmployee function

--- src/components/employees/EmployeeProvider.js
+++ src/components/employees/EmployeeProvider.js
@@ -11,7 +11,22 @@ export const EmployeeProvider = (props) => {
         .then(setEmployees)
     }
 
-    const addEmployee = () => {
-        
+    const addEmployee = employeeObj => {
+        return fetch("http://localhost:8088/employees", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(employeeObj)
+        })
+        .then(getEmployees)
     }
+
+    return (
+        <LocationContext.Provider value={{
+            employees, getEmployees, addEmployee
+        }}>
+            {props.children}
+        </LocationContext.Provider>
+    )
 }
